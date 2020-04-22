f43c6e805a244fa80affd2075653b9d9b2aa214b
Ryan Buchholz <rbuchholz425@gmail.com>
2020-04-22 16:06:08 -0600

    Tailored data to show relevance to subject.

--- script.js
+++ script.js
@@ -22,6 +22,13 @@ let fbIcon = L.icon({
 	popupAnchor:  [0, -30] // point from which the popup should open relative to the iconAnchor
 });
 
+let fbIcon2 = L.icon({
+	iconUrl: 'img/facebook2.svg',
+	iconSize:     [32, 37], // size of the icon
+	iconAnchor:   [16, 37], // point of the icon which will correspond to marker's location
+	popupAnchor:  [0, -30] // point from which the popup should open relative to the iconAnchor
+});
+
 let popup = L.popup();
 
 L.marker([35.08, -106.65], {icon:fbIcon}).addTo(map)
@@ -29,106 +36,43 @@ L.marker([35.08, -106.65], {icon:fbIcon}).addTo(map)
 
 //GeoJSON Sample code:
 
-let geojson = {
-	"type": "FeatureCollection",
-	"features": [
-		{
-			"type": "Feature",
-			"properties": {},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					-106.71295166015625,
-					35.24449753934067
-				]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					-106.62506103515625,
-					35.13900214724948
-				]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					-106.49322509765624,
-					35.185032937998294
-				]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					-106.62643432617186,
-					35.108674964507586
-				]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					-106.80084228515625,
-					35.146862906756304
-				]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					-106.6168212890625,
-					35.05698043137265
-				]
-			}
-		},
-		{
-			"type": "Feature",
-			"properties": {},
-			"geometry": {
-				"type": "Point",
-				"coordinates": [
-					-106.57562255859375,
-					35.40136418330354
-				]
-			}
-		}
-	]
-};
+let geojson = {"type": "FeatureCollection","features": [{"type": "Feature","properties": {},"geometry": {"type": "Point","coordinates": [-106.71295166015625,35.24449753934067]}},
+		{"type": "Feature","properties": {},"geometry": {"type": "Point","coordinates": [-106.62506103515625,35.13900214724948]}},
+		{"type": "Feature","properties": {},"geometry": {"type": "Point","coordinates": [-106.49322509765624,35.185032937998294]}},
+		{"type": "Feature","properties": {},"geometry": {"type": "Point","coordinates": [-106.62643432617186,35.108674964507586]}},
+		{"type": "Feature","properties": {},"geometry": {"type": "Point","coordinates": [-106.80084228515625,35.146862906756304]}},
+		{"type": "Feature","properties": {},"geometry": {"type": "Point","coordinates": [-106.6168212890625,35.05698043137265]}},
+		{"type": "Feature","properties": {},"geometry": {"type": "Point","coordinates": [-106.57562255859375,35.40136418330354]}}]};
 
-// styles each point marked.
-L.geoJSON(geojson, {
+//Formatting the geojson code below:
+//Styles each point marked.
+let sampleGeoJson = L.geoJSON(geojson, {
 	style :function(feature) {
 		return {
 
 		}
 	},
-	// adds the fb icon as the marker for each point.
+	//Adds the fb icon as the marker for each point.
 	pointToLayer: function(geoJsonPoint, latlng) {
 		return L.marker(latlng, {
 				icon: fbIcon
-			});
+			}).on('mousemove', function(e) {
+				e.target.setIcon(fbIcon2);
+		}).on('mouseout', function(e) {
+			e.target.setIcon(fbIcon);
+		});
 	},
-	// adds popups for each of the points.
+	//Adds popups for each of the points.
 	onEachFeature: function(feature, layer) {
 		if(feature.geometry.type==='Point') {
 			layer.bindPopup(feature.geometry.coordinates.join(','));
 		}
 	}
 }).addTo(map);
+
+let featureGroup = L.featureGroup({geojson}).addTo(map);
+
+//Bounds for the map to pull up with the data centered on it.
+map.fitBounds(featureGroup.getBounds(), {
+	padding: [100,100]
+});
