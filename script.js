6b113be981411d98d6d855bd39b10a44e67a2cb7
Ryan Buchholz <rbuchholz425@gmail.com>
2020-04-23 22:21:29 -0600

    Re-worked how the data will show, added dropdown that should eventually have data category options.

--- script.js
+++ script.js
@@ -10,6 +10,7 @@
 // 		{"type": "Feature","properties": {},"geometry": {"type": "Point","coordinates": [-106.6168212890625,35.05698043137265]}},
 // 		{"type": "Feature","properties": {},"geometry": {"type": "Point","coordinates": [-106.57562255859375,35.40136418330354]}}]};
 
+//JS for working button to toggle on and off the markers data
 $('#toggleLayer').click(function(){
 	if(map.hasLayer(face)){
 		map.removeLayer(face);
